65c406d02791d9d7a0b3528087988bf3538abb13
Javıer Esteban Guajardo Jorquera <javiergj@MacBook-Pro-de-Javer.local>
2021-09-23 18:57:15 +0200

    Adding basic components

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer(props) {
+    return (
+        <>
+            <footer>
+                Footer
+            </footer>
+            <style jsx>{`
+                footer { border: 1px solid #ccc }
+            `}</style>
+        </>
+    )
+}
